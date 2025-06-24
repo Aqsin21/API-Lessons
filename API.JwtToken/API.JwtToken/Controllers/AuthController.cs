2840e52a924af0fd100cae51439bef162561ad3d
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-06-24 19:53:52 +0400

    Yaqub

---
+++ API.JwtToken/API.JwtToken/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using API.JwtToken.Dtos;
+using API.JwtToken.Entities;
+using API.JwtToken.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.JwtToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuthController(AuthService authService, UserManager<AppUser> userManager)
+        {
+            _authService = authService;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.UserName);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+
+            var jwtResponseModel = await _authService.CreateToken(new JwtRequestModel
+            {
+                Username = user.Username
+               
+
+
+            });
+
+            return Ok(jwtResponseModel);
+        }
+    }
+}
---
+++ API.JwtToken/API.JwtToken/Dtos/JwtRequestModel.cs
@@ -0,0 +1,21 @@
+﻿namespace API.JwtToken.Dtos
+{
+    public class JwtRequestModel
+    {
+        public required string Username { get; set; }
+      
+    }
+
+    public class JwtResponseModel
+    {
+        public required string Token { get; set; }
+    }
+
+    public class JwtSettings
+    {
+        public required string Key { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public int DurationInMinutes { get; set; }
+    }
+}
